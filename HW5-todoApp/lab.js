bd10a7ccb9a7ffaa9f55bb71b78a2de8ec822b97
Bojidar Boradjiev <bojidarboradjiev@gmail.com>
2021-08-16 18:37:12 +0300

    todo app update

--- HW5-todoApp/lab.js
+++ HW5-todoApp/lab.js
@@ -1,113 +1,97 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // define variables and functions
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const displayTodoItemsCount = function() {
-	let count = todos.length || 0;
-	nodes.totalItemsCount.innerHTML = count;
-}
-const renderTodos = function(todo) {
-	// clean current todos:
-	nodes.todoListUL.innerHTML = '';
+const displayTodoItemsCount = function () {
+  let count = todos.length || 0;
+  nodes.totalItemsCount.innerHTML = count;
+};
+const renderTodos = function (todo) {
+  // clean current todos:
+  nodes.todoListUL.innerHTML = "";
 
-	// add todo item at the end
-	todo.forEach( (todo) => {
-		nodes.todoListUL.innerHTML += `
-		<li data-id=${todo .id}>
+  // add todo item at the end
+  todo.forEach((todo) => {
+    nodes.todoListUL.innerHTML += `
+		<li data-id=${todo.id}>
 			<span class="todoID">${todo.id}.</span>
-			<span class="${todo.completed?'completed':''}">${todo.title}</span>
+			<span class="${todo.completed ? "completed" : ""}">${todo.title}</span>
 			<div class="todo-remove-btn"><i class="far fa-trash-alt"></i></div>
 		</li>
 		`;
-	})
+  });
 
-	displayTodoItemsCount();
-}
-const addTodo = function() {
-	// get the input text:
-	const todoText = nodes.addTodoInput.value;
+  displayTodoItemsCount();
+};
+const addTodo = function () {
+  // get the input text:
+  const todoText = nodes.addTodoInput.value;
 
-	// make the ID - this should be done by the server:
-	const id = todos.length ? todos[todos.length-1].id + 1 : 1;
+  // make the ID - this should be done by the server:
+  const id = todos.length ? todos[todos.length - 1].id + 1 : 1;
 
-	const newTodo = {
-		"id": id,
-		"title": todoText,
-		"completed": false
-	};
+  const newTodo = {
+    id: id,
+    title: todoText,
+    completed: false,
+  };
 
-	// add new todo object to the end of todos array:
-	todos = [...todos, newTodo];
+  // add new todo object to the end of todos array:
+  todos = [...todos, newTodo];
 
-	// render todos:
-	renderTodos();
+  // render todos:
+  renderTodos();
 
-	// clear input text
-	nodes.addTodoInput.value = '';
+  // clear input text
+  nodes.addTodoInput.value = "";
 
-	// focus on input for new todo:
-	nodes.addTodoInput.focus();
-}
-const removeTodo = function(e) {
-	if(e.target.classList.contains('fa-trash-alt')){
-		// get id of todo to be removed:
-		let todoID= Number(e.target.parentNode.parentNode.dataset.id);
+  // focus on input for new todo:
+  nodes.addTodoInput.focus();
+};
+const removeTodo = function (e) {
+  if (e.target.classList.contains("fa-trash-alt")) {
+    // get id of todo to be removed:
+    let todoID = Number(e.target.parentNode.parentNode.dataset.id);
 
-		// get the index of todo to be removed from todos array:
-		let idx = todos.findIndex(todo => todo.id === todoID);
+    // get the index of todo to be removed from todos array:
+    let idx = todos.findIndex((todo) => todo.id === todoID);
 
-		// remove from todos array the element with index idx:
-		idx>=0 && todos.splice(idx,1);
+    // remove from todos array the element with index idx:
+    idx >= 0 && todos.splice(idx, 1);
 
-		// render todos:
-		renderTodos();
-	}
-}
+    // render todos:
+    renderTodos();
+  }
+};
 
-const getTodos = function() {
- const todos =  fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(renderTodos)
-  
-  return todos
+const getTodos = function () {
+  const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+  const serverUrl = "http://localhost:3000/todos";
+  const todos = fetch(serverUrl)
+    .then((response) => response.json())
+    .then(renderTodos);
 
-	// let todoStr = `
-	// 	[
-	// 		{
-	// 			"id":1,
-	// 			"title": "Todo1",
-	// 			"completed":false
-	// 		},
-	// 		{
-	// 			"id":2,
-	// 			"title": "Todo2",
-	// 			"completed":true
-	// 		}
-	// 	]
-	// `;
-
-	// return JSON.parse(todoStr);
-}
+  return todos;
+};
 
 // "cache" the DOM elements used in script:
 const nodes = {
-	'todoListUL': document.querySelector('ul.todo-items'),
-	'addTodoInput': document.querySelector('.todo-add>input'),
-	'addTodoBtn': document.querySelector('.todo-add-btn'),
-	'totalItemsCount': document.querySelector('.todos-total>.output')
-}
+  todoListUL: document.querySelector("ul.todo-items"),
+  addTodoInput: document.querySelector(".todo-add>input"),
+  addTodoBtn: document.querySelector(".todo-add-btn"),
+  totalItemsCount: document.querySelector(".todos-total>.output"),
+};
 
 // create array of todo objects from string
 let todos = getTodos();
-// console.log(todos);
-
+console.log(todos);
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // attach events
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-window.addEventListener('DOMContentLoaded', renderTodos);
+window.addEventListener("DOMContentLoaded", renderTodos);
 
 // add Todo Item (on button click):
-nodes.addTodoBtn.addEventListener('click', addTodo);
+nodes.addTodoBtn.addEventListener("click", addTodo);
 
 // remove Todo Item:
-nodes.todoListUL.addEventListener('click', removeTodo)
+nodes.todoListUL.addEventListener("click", removeTodo);
